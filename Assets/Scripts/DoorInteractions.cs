a48925c38f3dd4adf42d502574170bb4c61ffb48
Mehel Gupta <S118985@ltisdschools.net>
2025-04-23 12:05:17 -0500

    added tiles and background

--- Assets/Scripts/DoorInteractions.cs
+++ Assets/Scripts/DoorInteractions.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DoorInteractions : MonoBehaviour
 {
-    public GameObject button;
+    public GameObject door;
     public float interactDistance = 2f;
     private bool nearDoor = false;
     // Start is called before the first frame update
@@ -20,7 +20,7 @@ public class DoorInteractions : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
 
-            if(Vector2.Distance(transform.position, button.transform.position) < interactDistance)
+            if(Vector2.Distance(transform.position, door.transform.position) < interactDistance)
             {
                 nearDoor = true;
             }
